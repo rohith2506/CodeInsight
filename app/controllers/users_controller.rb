4a750e5b17b9050d7d0b08c6c4275dabc72c3188
Vani Vrat Srivastava <vanivrat111@gmail.com>
2014-03-06 16:29:33 +0530

    user new and creation basics done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
 		@user=User.new
 	end
 
+	def create
+		@user=User.new(user_params)
+		if @user.save
+			#handle a successful save
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:username,:name,:email,:password,:password_confirmation)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ CodeInsight::Application.routes.draw do
   
   resources :users
 
+  match '/signup',  to:'users#new', via:'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
