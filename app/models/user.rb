d816f5a232315b66035dc0f663147f1372def485
Vani Vrat Srivastava <vanivrat111@gmail.com>
2014-03-06 15:14:15 +0530

    Basic User model complete

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ActiveRecord::Base
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates( :username, presence: true , length: {maximum:50 },
+					uniqueness: {case_sensitive:false} )
+	validate :username_cannot_have_whitespaces
+	validates( :name, presence:true , length: {maximum:100} )
+	validates(:password, presence:true, length: {minimum:6} )
+	validates(:email, presence:true,
+				format: {with: VALID_EMAIL_REGEX},
+				uniqueness: {case_sensitive:false})
+
+	has_secure_password
+
+	def username_cannot_have_whitespaces
+		if(self.username==nil)
+			return
+		end
+
+		if(self.username.to_s.gsub(/\s+/, "").length==0)
+			return #since this has been checked in presence validation..
+		end
+		#s = "I have white space".delete(' ')
+		if(self.username.to_s.gsub(/\s+/, "").length != self.username.to_s.length )
+			errors.add(:username,"Cannot have whitespaces")
+		end  
+	end
+
+	
+end
