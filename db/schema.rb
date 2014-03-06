047b7d71ade0bac9edad1d45a26814dfcccca7b3
Vani Vrat Srivastava <vanivrat111@gmail.com>
2014-03-06 21:41:30 +0530

    add rating to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304190046) do
+ActiveRecord::Schema.define(version: 20140306120007) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140304190046) do
     t.string   "remember_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "rating"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
